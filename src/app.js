7336a1c57663c850dadd624907aaddd083ff04f9
OlhaAleks <106926651+OlhaAleks@users.noreply.github.com>
2022-07-26 17:04:48 +0300

    Added JS template

--- src/app.js
+++ src/app.js
@@ -20,6 +20,33 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function(day) {
+    forecastHTML = forecastHTML + 
+    `
+      <div class="col">
+        <div class="weather-forecast-date">
+        ${day}
+        </div>
+        <img src="images/mostly sunny.png" width="30"/>
+        <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">
+            18°
+        </span>
+        <span class="weather-forecast-temperature-min">
+             12°
+            </span>
+        </div>
+    </div>
+    `;
+  })
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
 
 function displayTemperature (response) {
     let temperatureElement = document.querySelector("#temperature");
@@ -82,3 +109,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("London");
+displayForecast();

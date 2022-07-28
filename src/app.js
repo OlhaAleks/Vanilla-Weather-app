d46064a157b53dc047cefb8845d73d08dd8e7abe
OlhaAleks <106926651+OlhaAleks@users.noreply.github.com>
2022-07-28 16:33:19 +0300

    Added request location

--- src/app.js
+++ src/app.js
@@ -121,6 +121,20 @@ function displayCelsiusTemperature(event) {
     temperatureElement.innerHTML = Math.round(celsiusTemperature);
     }
 
+function searchLocation(position) {
+  let apiKey = "5d30b474d4ae284fe49b962e45b136f7";
+  
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+
 let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
@@ -132,4 +146,7 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("London");
+let currentLocation = document.querySelector("#current-city");
+currentLocation.addEventListener("click", getCurrentLocation);
+
+search("Odessa");

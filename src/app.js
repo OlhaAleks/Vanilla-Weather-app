9d56caeb240bc5ddfb45d919e6c3b504ca74c127
OlhaAleks <106926651+OlhaAleks@users.noreply.github.com>
2022-07-28 13:02:29 +0300

    Changed style input

--- src/app.js
+++ src/app.js
@@ -64,10 +64,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "5d30b474d4ae284fe49b962e45b136f7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
